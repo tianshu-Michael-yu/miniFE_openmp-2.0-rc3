8628c4c95e783faf4501661deb0862a4a28f17c5
Tianshu Yu <tianshu.yu@duke.edu>
2023-10-23 09:56:16 -0400

    Init Finite Element

---
+++ basic/miniFE_info.hpp
@@ -0,0 +1,13 @@
+#ifndef miniFE_info_hpp
+#define miniFE_info_hpp
+
+#define MINIFE_HOSTNAME "kraken"
+#define MINIFE_KERNEL_NAME "'Linux'"
+#define MINIFE_KERNEL_RELEASE "'5.15.0-86-generic'"
+#define MINIFE_PROCESSOR "'x86_64'"
+
+#define MINIFE_CXX "'/usr/bin/g++'"
+#define MINIFE_CXX_VERSION "'g++ (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0'"
+#define MINIFE_CXXFLAGS "'-g'"
+
+#endif
